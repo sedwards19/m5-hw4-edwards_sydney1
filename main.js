3440d1a42a51e2469de669b3b0b5d9ca4a939ceb
Sydney Edwards <sydneyedwards@sydneys-air.attlocal.net>
2018-09-23 17:58:26 -0400

    Updated number branch

--- main.js
+++ main.js
@@ -3,3 +3,10 @@ console.log(myJavaScriptPractice);
 
 var num = 5;
 console.log(num);
+
+var numAddition = (5+5);
+console.log(numAddition);
+
+var numConcatOne = 2;
+var numConcatTwo = "3";
+console.log(numConcatOne+numConcatTwo);
